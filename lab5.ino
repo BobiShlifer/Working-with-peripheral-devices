16bc0849f33387ac99e1c4822f5a3a1b610a9696
Rob <73778095+BobiShlifer@users.noreply.github.com>
2022-04-16 00:59:32 +0300

    Add files via upload

---
+++ lab5.ino
@@ -0,0 +1,22 @@
+#include <rgb_lcd.h>
+rgb_lcd lcd;
+
+void printName(){  
+  lcd.setCursor(0,0);
+  lcd.print("Shlifer Robert");
+  lcd.setCursor(0,1);
+  lcd.print("GrNum ");
+}
+
+void setup() {  
+    lcd.begin(16, 2);  
+    Serial.begin (9600);     
+    printName();   
+}
+
+void loop() {   
+  delay(1000);
+  int result = analogRead(A1);  
+  lcd.setCursor(7,1);
+  lcd.print(result);  
+}
